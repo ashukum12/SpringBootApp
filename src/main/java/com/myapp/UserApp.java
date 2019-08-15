e16079cc7c192fbcc212fdca3c5b11f9850e23dd
Ashutosh Kumar <ashu.newtons@gmail.com>
2019-08-15 22:40:07 +0530

    user app

--- src/main/java/com/myapp/UserApp.java
+++ src/main/java/com/myapp/UserApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApp {
+public class UserApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApp.class, args);
+		SpringApplication.run(UserApp.class, args);
 
 	}
 
